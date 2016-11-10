97dfb1b2a37dbf8c7039651fbbe5b21caf2b3197
Marco Mangan <marco.mangan@pucrs.br>
2016-11-10 20:43:02 -0200

    Teste do Hello, World na IDE: Eclipse

---
+++ monopoly/src/main/java/br/pucrs/fds/monopoly/App.java
@@ -0,0 +1,13 @@
+package br.pucrs.fds.monopoly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
